4883bc0a045794c49010f2823728eb484b0d6548
Bogdan <bogdan7morozyuk@gmail.com>
2019-03-28 22:10:11 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+import './index.css';
+
+render(
+  <App />, 
+  document.getElementById('root')
+);
+
+serviceWorker.unregister();
