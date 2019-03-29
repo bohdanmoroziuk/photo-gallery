955423f7eef8b6fe681578dbd4ed44307a272cb2
Bogdan <bogdan7morozyuk@gmail.com>
2019-03-29 09:36:52 +0200

    Update component: App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import routes from './routes';
+import Layout from './components/layout/Layout';
+import RouterOutlet from './services/RouterOutlet';
+
 const App = () => (
   <div className="app">
-  
+    <Layout>
+      <RouterOutlet routes={routes} />
+    </Layout>
   </div>
 );
 
