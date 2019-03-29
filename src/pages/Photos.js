84bcb09859cb14d992ddf49e86b37799c4d1cba4
Bogdan <bogdan7morozyuk@gmail.com>
2019-03-29 11:21:01 +0200

    Update component: Photos

--- src/pages/Photos.js
+++ src/pages/Photos.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Alert, Button } from 'reactstrap';
+import { connect } from 'react-redux';
+import { selectPhotos } from '../redux/selectors';
 
-const Photos = () => (
-  <h2>Photos</h2>
+import PhotoList from '../components/PhotoList';
+
+const Photos = ({ photos, history }) => (
+  <div className="container mt-5">
+    {photos.length > 0 ? (
+      <PhotoList photos={photos} />
+    ) : (
+      <div>
+        <Alert color="light">
+          <h4 className="alert-heading">No photos</h4>
+          <p>Please, go back and select an album</p>
+          <hr />
+          <Button color="secondary" onClick={() => history.goBack()}>Back</Button>
+        </Alert>
+      </div>
+    )}
+  </div>
 );
 
-export default Photos;
+const mapStateToProps = state => ({
+  photos: selectPhotos(state),
+});
+
+export default connect(
+  mapStateToProps,
+)(Photos);
