bd9832b30a4eb20e9c3c3e0696bcc22a239895b6
Bogdan <bogdan7morozyuk@gmail.com>
2019-03-29 09:16:15 +0200

    Configure routing

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+const Index = () => (
+  <Router>
+    <App />
+  </Router>
+);
+
 render(
-  <App />, 
+  <Index />, 
   document.getElementById('root')
 );
 
