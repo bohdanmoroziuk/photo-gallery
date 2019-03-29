4e182952fa6e0ca1e6f6ec419611e2d50af51541
Bogdan <bogdan7morozyuk@gmail.com>
2019-03-29 09:13:23 +0200

    Create blank pages

---
+++ src/pages/Albums.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Albums = () => (
+  <h2>Albums</h2>
+);
+
+export default Albums;
---
+++ src/pages/Photos.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Photos = () => (
+  <h2>Photos</h2>
+);
+
+export default Photos;
